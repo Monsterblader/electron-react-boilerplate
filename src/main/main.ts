12cfca8a146b035faa2edb3bf99346633c8ff468
Monsterblader <Monsterblader@gmail.com>
2022-07-05 11:05:43 -0700

    Sets up NodeMailer.

--- src/main/main.ts
+++ src/main/main.ts
@@ -14,6 +14,7 @@ import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
+import NodeMailer from './NodeMailer';
 
 class AppUpdater {
   constructor() {
@@ -135,3 +136,7 @@ app
     });
   })
   .catch(console.log);
+
+if (false) {
+  NodeMailer().catch(console.error);
+}
